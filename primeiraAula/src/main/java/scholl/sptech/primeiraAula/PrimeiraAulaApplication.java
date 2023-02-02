39c3e5bae5c00bbd19c58eebdd8d2e3c6a47360f
IvanCCO <ivan.miranda@sptech.school>
2023-02-02 20:47:23 -0300

    Aula 1- SpringBoot

---
+++ primeiraAula/src/main/java/scholl/sptech/primeiraAula/PrimeiraAulaApplication.java
@@ -0,0 +1,13 @@
+package scholl.sptech.primeiraAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraAulaApplication.class, args);
+	}
+
+}
