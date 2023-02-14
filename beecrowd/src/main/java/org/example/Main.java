19f423e7637ece0c56b3b8a1d80b2966ffd1b132
Ivan Freire <102231864+IvanCCO@users.noreply.github.com>
2023-02-14 13:49:28 -0300

    Atualizando

---
+++ beecrowd/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        // Quantidade de testes
+
+        int teste = in.nextInt();
+
+
+
+
+        }
+
+
+
+    }
+}
