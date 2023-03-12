b338c0814b8f48169c3a1fa62bd0b75c18ce594a
Ivan Freire <102231864+IvanCCO@users.noreply.github.com>
2023-03-12 12:14:20 -0300

    ajuda ai

---
+++ login-02221071-ivanmiranda/src/test/java/school/sptech/login02221071ivanmiranda/Login02221071IvanmirandaApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.login02221071ivanmiranda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login02221071IvanmirandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
